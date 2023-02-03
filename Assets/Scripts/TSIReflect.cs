ea1234116633ab0005cb5bc824432a54974b98a5
Aniket Rajnish <aniket.r@iitgn.ac.in>
2023-02-03 17:33:04 +0530

    reflect mov

--- Assets/Scripts/TSIReflect.cs
+++ Assets/Scripts/TSIReflect.cs
@@ -2,22 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TSIController : MonoBehaviour
+public class TSIReflect : MonoBehaviour
 {
     Rigidbody2D rb;
     Vector2 movement;
     Vector2 prevpos;
-    [HideInInspector] public bool isSlowMo;
 
     [Header("Assign These:")]
     public GameObject sprite;
     public float rotSpeed, speed;
-    public static TSIController tsiInstance;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        tsiInstance = this;
     }
     void Update()
     {
@@ -26,9 +23,9 @@ public class TSIController : MonoBehaviour
         float movX = Input.GetAxisRaw("Horizontal");
         float movY = Input.GetAxisRaw("Vertical");
 
-        movement = new Vector2(movX, movY);
+        movement = new Vector2(-movX, movY);
 
-        rb.MovePosition(rb.position + movement.normalized * speed * Time.deltaTime / Time.timeScale);        
+        rb.MovePosition(rb.position + movement.normalized * speed * Time.deltaTime / Time.timeScale);
 
         Vector2 movedir = rb.position - prevpos;
 
@@ -36,8 +33,7 @@ public class TSIController : MonoBehaviour
             sprite.transform.rotation = Quaternion.Lerp(sprite.transform.rotation,
                 Quaternion.Euler(0, 0, Mathf.Rad2Deg * Mathf.Atan2(movedir.y * 100, movedir.x * 100) - 90),
                 rotSpeed * Time.deltaTime / Time.timeScale);
-
-        isSlowMo = (movX != 0 || movY != 0) ? true : false;
+        
         prevpos = rb.position;
     }
 }
