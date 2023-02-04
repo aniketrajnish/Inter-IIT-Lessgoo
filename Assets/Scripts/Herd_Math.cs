96170e646d7914461f92b5a03cff6084d222b5d5
ajiteshjoshi <60251609+ajiteshjoshi@users.noreply.github.com>
2023-02-05 04:25:23 +0530

    Level

---
+++ Assets/Scripts/Herd_Math.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Herd
+{
+    [SerializeField] private GameObject creature;
+    [SerializeField] private int number;
+    [SerializeField] private Transform centre;
+    [SerializeField] private float radius;
+
+
+    public void Instantiate_Herd()
+    {
+
+    }
+
+    
+}
