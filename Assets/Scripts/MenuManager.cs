b40aa9491f44c2f5c3e5cca455b76e22c98235ab
Aniket Rajnish <aniket.r@iitgn.ac.in>
2023-02-06 17:25:21 +0530

    obs

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Camera[] cams;
+    float initX1, initY1, initX2, initY2;
+    [SerializeField] float freq, amp;
     void Start()
     {
-        
+        cams = GameObject.FindObjectsOfType<Camera>();
+
+        initX1 = cams[0].transform.position.x;
+        initY1 = cams[0].transform.position.y;
+
+        initX2 = cams[1].transform.position.x;
+        initY2 = cams[1].transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float smoothX = amp * Mathf.Sin(Time.unscaledTime * freq);
+        float smoothY = -.5f* amp * Mathf.Cos(Time.unscaledTime * freq);
+
+        cams[0].transform.position = new Vector3(smoothX + initX1, smoothY + initY1, -10);
+        cams[1].transform.position = new Vector3(-smoothX + initX2, -smoothY + initY2, -10);
+    }
+    private void OnDisable()
+    {
+        cams[0].transform.position = new Vector3(initX1, initY1, -10);
+        cams[1].transform.position = new Vector3(initX2, initY2, -10);
     }
 }
